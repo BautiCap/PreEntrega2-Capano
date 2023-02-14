a011c76db4cb2f9462d4f0ea821f2a9d7a43e8a8
BautiCap <bautidragon@hotmail.com>
2023-02-14 19:17:32 -0300

    version 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,59 @@
 
-let productos = []
-let carroDeCompras = [];
-
-class ingProductos {
-    constructor(nombre,precio,stockD){
+class Productos {
+    constructor(nombre,precio,cantidad){
         this.name=nombre;
         this.price=precio;
-        this.stock=stockD;
-        this.agregarS=function(s){
-            this.stock += s  
-        }
-        this.agregarVidr=function(){
-            if(this.stock > 0){
-                productos.push(this)
-            }
-        }
-
+        this.stock=cantidad;
     }
 }
-const producto0 = new ingProductos ("botas",400,1)
-const producto1 = new ingProductos ("sandalias",300,0)
-const producto2 = new ingProductos ("bolso",1000,2)
-const producto3 = new ingProductos ("brazalete",500,1)
+const Stock = [
+    new Productos ("botas",400,1),
+    new Productos ("sandalias",300,0),
+    new Productos ("bolso",1000,2),
+    new Productos ("brazalete",500,1)
+]
 
-producto0.agregarS(1)
-producto0.agregarVidr()
-producto1.agregarVidr()
-producto2.agregarVidr()
-producto3.agregarVidr()
+const costo = [];
+const carrito = [];
 
-carrito = ()=>{
-    let listaProductos = productos.map((Producto2)=> Producto2.name + " " + Producto2.price + "$");
-    alert("Hola, a continuacion te mostramos nuestra lista de productos:")
-    alert(listaProductos.join(" - "))
-    while(true){ 
-        let seleccion = prompt("Que producto quiere agregar al carrito?")
-        if(listaProductos.includes(seleccion)){
-            for(let i = 0;i <= listaProductos.length;i++){
-                if(seleccion == listaProductos[i] && productos[i].stock > 0){
-                    carroDeCompras.push(listaProductos[i]);
+
+funcion = ()=>{
+    let mensaje = ""
+    for(let p of Stock){mensaje += `- ${p.name} (${p.stock} disponibles) - ${p.price}$ la unidad\n`}
+    alert(`Hola, a continuacion te mostramos nuestra lista de productos:\n\n${mensaje}`)
+    
+    while(true){  
+        let seleccion = prompt("Que producto quiere agregar al carrito?");    
+        for(let h = 0;h < Stock.length;h++){  
+            if(Stock[h].name == seleccion && Stock[h].stock > 0){
+                switch(seleccion){
+                    case Stock[h].name:
+                    carrito.push(Stock[h].name);
+                    costo.push(Stock[h].price)
+                    costoTotal = costo.reduce((acc, p)=> acc + p, 0);
                     alert(`Su producto: ${seleccion}, se agrego correctamente al carrito.`);
-                    productos[i].stock -= 1;
+                    break
                 }
-                else if(productos[i].stock == 0){
-                    alert("No hay mas stock de este producto")
-                }        
+                repetir = prompt("¿Desea agregar otro producto al carro?");
+                if(repetir == "si" || repetir == "SI"){
+                    true
+                }
+                else if(repetir == "no" || repetir == "NO"){
+                    carrito.join(" - ")
+                    document.write(`Su carrito de compras tiene los siguientes productos:<b style=color:red>${carrito}</b>.<br>
+                    El precio total es de: <b style=color:blue>${costoTotal}$</b>`)
+                    break
+                }                      
             }
-            repetir = prompt("¿Desea agregar otro producto al carro?");
-            if(repetir == "si" || repetir == "SI"){
-                true
-            }
-            else{
-                let total = `Su carrito de compras tiene los siguientes productos:<b style=color:red>${carroDeCompras}</b>.<br>`;
-                document.write(total)
-                break
-            }    
+            else if(Stock[h].name == seleccion && Stock[h].stock == 0){
+                alert("No hay mas stock de este producto") 
+            }         
         }
-        else{
-            alert("Error: Porfavor ingrese un producto existente")
+        if(repetir == "no" || repetir == "NO"){
+            break
         }
     }
 }
 
-carrito();
+funcion();
+
