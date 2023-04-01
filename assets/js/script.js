c99d4d663836b34fd98784974ea31bddbbb067c7
BautiCap <bautidragon@hotmail.com>
2023-04-01 08:32:02 -0300

    prueba5

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ for (elem of Stock){
     contenedor.innerHTML += `
     <div class="col-md-4 my-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1000">
     <div class="card text-center" style="width: 18rem">
-    <img class="card-img-top mt-2 img-thumbnail" src="./assets/img/${img}" alt="${name}">
+    <img class="card-img-top mt-2 img-thumbnail" src="../assets/img/${img}" alt="${name}">
     <div class="card-body">
       <h5 class="card-title">${name}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
